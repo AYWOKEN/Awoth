c31316e894e96e756e9f28c9ed8ee532defb3d37
AYWOKEN <dev-awoken@hotmail.com>
2018-07-17 13:36:51 +0200

    Resolved problem in /login

--- app.js
+++ app.js
@@ -2,22 +2,26 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const { promisify } = require('util')
+const {
+    promisify
+} = require('util')
 const argon2 = require('argon2');
 
 
-writeFile = promisify(fs.writeFile)
-
+const writeFile = promisify(fs.writeFile)
+const readFile = promisify(fs.readFile)
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.send("Bienvenue sur mon système d'authentification ! :)");
 });
 
-app.get('/inscription', function (req, res) {
+app.get('/inscription', function(req, res) {
     res.render('inscription');
 })
 
@@ -29,44 +33,35 @@ app.post('/inscription', async (req, res) => {
     await writeFile(`${req.body.user_pseudo}.json`, JSON.stringify({
         ...req.body,
         pass: await argon2.hash(req.body.pass)
-      }, false, 4))
-  
+    }, false, 4))
 
 
-      res.render('inscription_succes')
-  })
+
+    res.render('inscription_succes')
+})
 
 
-app.get('/login', function (req, res) {
+app.get('/login', function(req, res) {
     res.render('login');
 })
 
 
 
 
-
-app.post('/login', function (req, res) {
-
-
-
-    fs.readFile(req.body.user_pseudo + ".json", (err, data) => {    
-      if (err) {
-        return res.render('login_failed')
-      }
- 
-      const user = JSON.parse(data)
-      
-      if ((req.body.pass == user.pass) && (req.body.user_pseudo == user.user_pseudo)) {        
-        if (err) {
-            console.log(err)
-           res.render('login_failed')
+app.post('/login', async (req, res) => {
+    try {
+        const data = await readFile(req.body.user_pseudo + ".json")
+        const user = JSON.parse(data)
+        if (await argon2.verify(user.pass, req.body.pass)) {
+            res.render('login_success')
+        } else {
+            res.render('login_failed')
         }
-     } else {
-          res.render('login_success');
-      }
-      
-    })
-  })
+    } catch (err) {
+
+        res.render('login_failed')
+    }
+})
 
 app.listen(3000);
 console.log('Application en marche !');
